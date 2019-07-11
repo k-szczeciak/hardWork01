81b2c074ff760ac75a8964362a4216972ba239b5
k-szczeciak <szczeciak@hotmail.com>
2019-07-11 23:49:06 +0200

    min val

--- src/main02.java
+++ src/main02.java
@@ -2,5 +2,23 @@ public class main02 {
 
     public static void main(String[] args) {
         System.out.println("test");
+
+        int[] input = {1,2,3,0,5,6,7,9};
+        System.out.println(findMin(input));
+        findMin(input);
+
+    }
+
+    public static int findMin(int[] arr){
+        //finding max
+        int result = arr[0];
+        int pos = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < result ){
+                result = arr[i];
+                pos = i;
+            }
+        }
+        return result;
     }
 }
