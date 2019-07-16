5d6d0d2bf5041c9cc6b545b3f9557977a0c20299
k-szczeciak <szczeciak@hotmail.com>
2019-07-16 07:24:38 +0200

    second task findind second smallest number

--- src/main02.java
+++ src/main02.java
@@ -1,27 +1,48 @@
 public class main02 {
 
     public static void main(String[] args) {
-        System.out.println("test");
 
-        int[] input = {1,2,3,0,5,6,7,9};
-        System.out.println(findMin(input));
+        int[] input = {11,21,53,4,15,12,17,9};
+        System.out.println("smallest number: " + findMin(input));
+        System.out.println("second smallest number: " + findSecondSmallest(input));
 
 
     }
 
+    //finding smallest number in array
     public static int findMin(int[] arr){
-        //finding max
+        //finding min, 0-th smallest element in table
         int result = arr[0];
         int pos = 0;
         for(int i = 0; i < arr.length; i++){
             if(arr[i] < result ){
                 result = arr[i];
                 pos = i;
-                //new results
+                System.out.println("position of smallest number: " + pos);
             }
         }
         return result;
     }
 
-    public int result;
+    //int[] input = {11,21,53,4,5,12,7,9};
+    //finding second smallest number in array
+    public static int findSecondSmallest(int[] arr){
+        int smallest = arr[0];
+        int secondSmallest = smallest;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < smallest){
+                smallest = arr[i];
+            }
+            if (arr[i] < secondSmallest && arr[i] > smallest){
+               secondSmallest = arr[i];
+            }
+        }
+
+
+        return secondSmallest;
+    }
+
+
+    //public int result;
 }
